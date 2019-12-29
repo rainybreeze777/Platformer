3a75514492d7411247989c40e04e456b89599eab
Siting Pan <psting3456@gmail.com>
2019-12-29 13:04:27 -0500

    Fix bug player not reacting to moving platforms

--- Assets/Src/SelfMovableObject.cs
+++ Assets/Src/SelfMovableObject.cs
@@ -5,58 +5,56 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class SelfMovableObject : Triggerable
 {
+  public bool m_ShouldMove = false;
   public bool m_OneTimeTrigger = true;
   public bool m_Loop = false;
   public EDirection m_InitialDirection = EDirection.Positive;
-  public Vector3 m_Pos1;
-  public Vector3 m_Pos2;
+  public Vector2 m_Pos1;
+  public Vector2 m_Pos2;
   [Range(0, 1)]
   public float m_StartingPos;
   public float m_Speed;
 
-  private bool m_ShouldMove = false;
   private bool m_AlreadyTriggered = false;
   private float m_Dist;
   private int m_CurrentDirection;
   private float m_T;
+  private Vector2 m_Velocity;
+  private Rigidbody2D m_Rb2d;
 
   // Start is called before the first frame update
-  void Start()
-  {
-    m_Dist = Vector3.Distance(m_Pos1, m_Pos2);
+  void Start() {
+    m_Rb2d = GetComponent<Rigidbody2D>();
+    m_Dist = Vector2.Distance(m_Pos1, m_Pos2);
     m_CurrentDirection = m_InitialDirection == EDirection.Positive ? 1 : -1;
     m_T = m_StartingPos;
-    transform.position = Vector3.Lerp(m_Pos1, m_Pos2, m_T);
+    m_Rb2d.position = Vector2.Lerp(m_Pos1, m_Pos2, m_T);
+    Debug.Assert(tag == "SelfMovable", name + " object is self movable, but does not have tag SelfMovable!");
   }
 
   // Update is called once per frame
-  void Update()
-  {
+  void FixedUpdate() {
     if (!Application.IsPlaying(gameObject)) {
-      transform.position = Vector3.Lerp(m_Pos1, m_Pos2, m_StartingPos);
+      m_Rb2d.position = Vector2.Lerp(m_Pos1, m_Pos2, m_StartingPos);
       return;
     }
     if (m_ShouldMove) {
+      float prevT = m_T;
       m_T += (Time.deltaTime * m_Speed / m_Dist) * m_CurrentDirection;
-      if (m_Loop)
-      {
-        if (m_T < 0)
-        {
-          m_CurrentDirection = 1;
-          m_T = 0 - m_T;
+      if (m_Loop) {
+        // Reposition out-of-bounds t and flip the direction
+        if (m_T < 0 || m_T > 1) {
+          m_T = m_CurrentDirection + 1 - m_T;
+          m_CurrentDirection *= -1;
         }
-        else if (m_T > 1)
-        {
-          m_CurrentDirection = -1;
-          m_T = 2 - m_T;
-        }
-      }
-      else
-      {
+      } else {
         m_T = Mathf.Clamp(m_T, 0, 1);
       }
+      if (m_T != prevT) {
+        m_Velocity = (m_Pos2 - m_Pos1).normalized * m_CurrentDirection * m_Speed;
+      }
     }
-    transform.position = Vector3.Lerp(m_Pos1, m_Pos2, m_T);
+    m_Rb2d.position = Vector3.Lerp(m_Pos1, m_Pos2, m_T);
   }
 
   public override void NotifyTriggerEnter() {
@@ -71,6 +69,8 @@ public class SelfMovableObject : Triggerable
       m_ShouldMove = !m_ShouldMove;
     }
   }
+
+  public Vector2 Velocity { get { return m_Velocity; } }
 
   public enum EDirection { Positive, Negative };
 }
