c772514656e8b7666b73337e236059d162630a68
Siting Pan <psting3456@gmail.com>
2020-01-02 17:54:48 -0500

    Implement unlockable lever

--- Assets/Src/UI/UIInventoryListener.cs
+++ Assets/Src/UI/UIInventoryListener.cs
@@ -7,11 +7,22 @@ using UnityEngine.UI;
 public class UIInventoryListener : MonoBehaviour {
   
   public EventManager m_EventManager;
-
+  // For Callbacks, watch out for thread synchronization
+  // TODO: Get a lock maybe? Or a queue of some kind, to avoid
+  // overwrites
   private InvtItem m_CacheItem;
+  // Dictionary to store quick UI object look up
+  // Key: ItemId
+  // Value: UI GameObject
+  private Dictionary<string, GameObject> m_ItemsDict;
+
+  void Awake() {
+    m_ItemsDict = new Dictionary<string, GameObject>();
+  }
 
   void Start() {
     m_EventManager.AddListener<ObtainItemUEvent, InvtItem>(OnObtainItem);
+    m_EventManager.AddListener<SpendItemUEvent, InvtItem>(OnSpendingItem);
   }
 
   public void OnObtainItem(InvtItem item) {
@@ -22,7 +33,14 @@ public class UIInventoryListener : MonoBehaviour {
       image.preserveAspect = true;
       image.sprite = m_CacheItem.Sprite;
       imageObj.transform.SetParent(gameObject.transform);
+      m_ItemsDict[item.Id] = imageObj;
       m_CacheItem = null;
     };
+  }
+
+  public void OnSpendingItem(InvtItem item) {
+    m_ItemsDict[item.Id].transform.SetParent(null);
+    Destroy(m_ItemsDict[item.Id]);
+    m_ItemsDict.Remove(item.Id);
   }
 }
