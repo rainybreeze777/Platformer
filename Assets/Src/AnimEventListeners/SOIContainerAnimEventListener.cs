95c7b59528d94b1043d57c636a008470bb802935
Siting Pan <psting3456@gmail.com>
2020-04-13 19:13:46 -0400

    Add coffer with animation and game logic

---
+++ Assets/Src/AnimEventListeners/SOIContainerAnimEventListener.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Scene Obtainable Item Container AnimEventListener
+public class SOIContainerAnimEventListener : AnimEventListener
+{
+  private SceneObtainableItemContainer m_Container;
+
+  void Start() {
+    m_Container = GetComponentInParent<SceneObtainableItemContainer>()
+                    as SceneObtainableItemContainer;
+  }
+
+  void OnContainerOpen() {
+    m_Container.PopItem();
+  }
+}
---
+++ Assets/Src/SceneObtainableItemContainer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+[RequireComponent(typeof(Collider2D))]
+public class SceneObtainableItemContainer : MonoBehaviour
+{
+  public AssetReference m_ContainedItemPrefab;
+  public Vector2 m_ItemPopVector;
+  public float m_ItemPopForce = 1.0f;
+
+  private Animator m_ContainerAnimator;
+  private Transform m_MainFrame;
+  private bool m_ContainerOpened = false;
+
+  void Start() {
+    m_ContainerAnimator = GetComponentInChildren<Animator>() as Animator;
+    m_MainFrame = GameObject.FindWithTag("MainFrame")
+                    .GetComponent<Transform>() as Transform;
+  }
+
+  void OnTriggerEnter2D(Collider2D collider) {
+    if (m_ContainerOpened) { return; }
+    if (collider.tag == "Player") {
+      m_ContainerOpened = true;
+      m_ContainerAnimator.SetTrigger("shouldOpen");
+    }
+  }
+
+  public void PopItem() {
+    m_ContainedItemPrefab
+      .InstantiateAsync(transform.position, Quaternion.identity, m_MainFrame)
+      .Completed += (asyncRes) => {
+        asyncRes.Result.GetComponent<Rigidbody2D>()
+                       .AddForce(m_ItemPopVector.normalized * m_ItemPopForce
+                                 , ForceMode2D.Impulse);
+      };
+  }
+}
