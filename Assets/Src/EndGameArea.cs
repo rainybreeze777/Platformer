6f695221a9b2d19f7561b754fef700e71228b38d
Siting Pan <psting3456@gmail.com>
2020-05-15 17:47:24 -0400

    Add End game play movie

---
+++ Assets/Src/EndGameArea.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using Input = Platformer.Input;
+
+[RequireComponent(typeof(Collider2D))]
+public class EndGameArea : MonoBehaviour
+{
+  public MovieController m_MovieController;
+  public VideoPlayer m_EndGameClip;
+
+  void OnTriggerEnter2D(Collider2D collider) {
+    Input.AllowInput = false;
+    m_MovieController.PlayMovie(m_EndGameClip, null, () => {
+      Application.Quit();
+    });
+  }
+}
