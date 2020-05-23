cccd0c69035d8bfbe331cae372c5f98b81c51499
Siting Pan <psting3456@gmail.com>
2020-05-23 18:46:55 -0400

    Adjust hidden items and remove pressZ area effect
    Now PressZ tip will always play loop animation, instead of responding to
    player position. This is to reduce the error chance of DragonBones
    throwing exceptions.

--- Assets/Src/AnimationDrivers/PressZAnimDriver.cs
+++ Assets/Src/AnimationDrivers/PressZAnimDriver.cs
@@ -3,25 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using DragonBones;
 
-[RequireComponent(typeof(ShowTipWhenInZone))]
+// [RequireComponent(typeof(ShowTipWhenInZone))]
 public class PressZAnimDriver : AnimDriver
 {
-  private ShowTipWhenInZone m_ShowTip;
+  // private ShowTipWhenInZone m_ShowTip;
 
   private const string kLoop = "Loop";
   private const string kStatic = "Static";
   private const string kDisappear = "Disappear";
 
-  private DragonBones.AnimationState m_LoopState;
+  // private DragonBones.AnimationState m_LoopState;
 
   // Start is called before the first frame update
   public override void Start()
   {
     base.Start();
-    m_ShowTip = GetComponent<ShowTipWhenInZone>();
-    m_Anim.GotoAndPlayByProgress(kDisappear, 100.0f, 1);
+    // m_ShowTip = GetComponent<ShowTipWhenInZone>();
+    // m_Anim.GotoAndPlayByProgress(kDisappear, 100.0f, 1);
+    m_Anim.Play(kLoop);
   }
 
+/*
   public void PlayLoop() {
     if (m_LoopState == null) {
       m_LoopState = m_Anim.FadeIn(kLoop);
@@ -34,12 +36,14 @@ public class PressZAnimDriver : AnimDriver
       m_Anim.GotoAndPlayByTime(kLoop, m_LoopState.currentTime, 1);
     }
   }
-
+*/
   protected override void OnAnimationComplete(string type, EventObject evObj) {
+    /*
     DragonBones.AnimationState animState = evObj.animationState;
     if (animState.name.Equals(kLoop)) {
       m_LoopState = null;
       m_Anim.FadeIn(kDisappear, m_FadeInTime, 1);
     }
+    */
   }
 }
